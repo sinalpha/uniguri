5fccc9030f1eb728e804c401ccb64b92681d71b9
sin178deg <59336397+sinalpha@users.noreply.github.com>
2024-06-20 02:06:11 +0000

    test build

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
 {
     // Register the window class.
     const wchar_t CLASS_NAME[]  = L"Sample Window Class";
